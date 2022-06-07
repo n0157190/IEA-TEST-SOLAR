021a879e68c07b9b75118dd4c316c4e9e4c01f03
Bryant_Beach <101576438+n0157190@users.noreply.github.com>
2022-06-07 15:38:19 -0400

    Update solar_test.py

--- solar_test.py
+++ solar_test.py
@@ -6,6 +6,6 @@ def test_get_observer_location():
     val = get_observer_location()
     assert val == '39.0438 -77.4874'
 
-def test_print_position('123.45', '64.78'):
-    val = print_position()
+def test_print_position():
+    val = print_position('123.45', '64.78')
     assert val == 'The Sun is currently at: 123.45 degrees azimuth 64.78 degrees altitude'
