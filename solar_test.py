1ac5187c1dbd778902ef3f49c113576aa406b88c
Bryant Beach <bryant.beach@libertymutual.com>
2022-06-07 15:35:22 -0400

    Adding latest solar changes

--- solar_test.py
+++ solar_test.py
@@ -5,3 +5,7 @@ import solar
 def test_get_observer_location():
     val = get_observer_location()
     assert val == '39.0438 -77.4874'
+
+def test_print_position('123.45', '64.78'):
+    val = print_position()
+    assert val == 'The Sun is currently at: 123.45 degrees azimuth 64.78 degrees altitude'
